f0c515fe88ed3953637c5761b13fea56f59502f7
kOidzi <vasya24.07@mail.ru>
2025-12-07 08:20:29 +0300

    разделил js на методы + точки

--- static/index.js
+++ static/index.js
@@ -1,25 +1,26 @@
-const canva = function canva(){
-    const RGeom = 200,
-          sixCanv = 6,
-          twoCanv = 2,
-          zeroCanv = 0,
-     canvases = document.getElementsByTagName('canvas'),
-     canvas = canvases[zeroCanv],
-          ctx = canvas.getContext('2d'),
-          { height, width } = canvas,
-          centerX = width / twoCanv,
-          centerY = height / twoCanv;
+const scale = 200,
+    sixCanv = 6,
+    twoCanv = 2,
+    zeroCanv = 0,
+    canvases = document.getElementsByTagName('canvas'),
+    canvas = canvases[zeroCanv],
+    ctx = canvas.getContext('2d'),
+    { height, width } = canvas,
+    centerX = width / twoCanv,
+    centerY = height / twoCanv;
+
+canva = function canva(){
     ctx.fillStyle = 'rgba(51, 153, 255, 0.2)';
     ctx.beginPath();
-    ctx.rect(centerX - RGeom / twoCanv, centerY - RGeom, RGeom / twoCanv, RGeom);
+    ctx.rect(centerX - scale / twoCanv, centerY - scale, scale / twoCanv, scale);
     ctx.fill();
     ctx.moveTo(centerX, centerY);
-    ctx.arc(centerX, centerY, RGeom, Math.PI/twoCanv, Math.PI, false);
+    ctx.arc(centerX, centerY, scale, Math.PI/twoCanv, Math.PI, false);
     ctx.fill();
     ctx.beginPath();
     ctx.moveTo(centerX, centerY)
-    ctx.lineTo(centerX + RGeom, centerY);
-    ctx.lineTo(centerX, centerY + RGeom / twoCanv);
+    ctx.lineTo(centerX + scale, centerY);
+    ctx.lineTo(centerX, centerY + scale / twoCanv);
     ctx.closePath();
     ctx.fill();
     ctx.beginPath();
@@ -31,16 +32,85 @@ const canva = function canva(){
     ctx.stroke();
     ctx.font = "12px monospace";
     ctx.strokeText("0", centerX + sixCanv, centerY - sixCanv);
-    ctx.strokeText("R/2", centerX + RGeom / twoCanv, centerY - sixCanv);
-    ctx.strokeText("R", centerX + RGeom, centerY - sixCanv);
-    ctx.strokeText("-R/2", centerX - RGeom / twoCanv, centerY - sixCanv);
-    ctx.strokeText("-R", centerX - RGeom, centerY - sixCanv);
-    ctx.strokeText("R/2", centerX + sixCanv, centerY - RGeom / twoCanv);
-    ctx.strokeText("R", centerX + sixCanv, centerY - RGeom);
-    ctx.strokeText("-R/2", centerX + sixCanv, centerY + RGeom / twoCanv);
-    ctx.strokeText("-R", centerX + sixCanv, centerY + RGeom);
+    ctx.strokeText("R/2", centerX + scale / twoCanv, centerY - sixCanv);
+    ctx.strokeText("R", centerX + scale, centerY - sixCanv);
+    ctx.strokeText("-R/2", centerX - scale / twoCanv, centerY - sixCanv);
+    ctx.strokeText("-R", centerX - scale, centerY - sixCanv);
+    ctx.strokeText("R/2", centerX + sixCanv, centerY - scale / twoCanv);
+    ctx.strokeText("R", centerX + sixCanv, centerY - scale);
+    ctx.strokeText("-R/2", centerX + sixCanv, centerY + scale / twoCanv);
+    ctx.strokeText("-R", centerX + sixCanv, centerY + scale);
+}
+const checkPoint = async function checkPoint(){
+        const newRow = table.insertRow(CELL_INDEX.APPEND),
+        rowX = newRow.insertCell(CELL_INDEX.XIndex),
+        rowY = newRow.insertCell(CELL_INDEX.YIndex),
+        rowR = newRow.insertCell(CELL_INDEX.RIndex),
+        rowCurrentTime = newRow.insertCell(CELL_INDEX.CURRENT_TIME),
+        rowTime = newRow.insertCell(CELL_INDEX.TIME),
+        rowResult = newRow.insertCell(CELL_INDEX.RESULT),
+        results = { 
+            resX: state.siteX,
+            resY: state.siteY,
+            resR: state.siteR,
+            execTime: "",
+            currentTime: "",    
+            result: false,  
+        };
+        try{
+        const params = new URLSearchParams(state),
+        response = await fetch("/fcgi-bin/labwork1.jar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: params.toString()
+        });
+        if (response.ok){
+            const result = await response.json();
+            results.currentTime = new Date(result.now).toLocaleString();
+            results.execTime = `${result.time} ns`
+            results.result = result.result.toString();
+            printPoint(state.siteX, state.siteY, state.siteR, result.result);
+        } else if (response.status === bedIndex) {
+            const result = await response.json();
+            results.currentTime = new Date(result.now).toLocaleString();
+            results.execTime = "N/A";
+            results.result = `error: ${result.reason}`;
+            printPoint(state.siteX, state.siteY, state.siteR, false);
+        } else {
+            results.currentTime = "N/A";
+            results.execTime = "N/A";
+            results.result = "error";
+            printPoint(state.siteX, state.siteY, state.siteR, false);
+        }
+        rowX.innerText = results.resX.toString();
+        rowY.innerText = results.resY.toString();
+        rowR.innerText = results.resR.toString();
+        rowCurrentTime.innerText = results.currentTime;
+        rowTime.innerText = results.execTime;
+        rowResult.innerText = results.result;
+        } catch{
+            rowX.innerText = results.resX.toString();
+            rowY.innerText = results.resY.toString();
+            rowR.innerText = results.resR.toString();
+            rowCurrentTime.innerText = "N/A";
+            rowTime.innerText = "N/A";
+            rowResult.innerText = "N/A";
+            printPoint(state.siteX, state.siteY, state.siteR, false);
+        }
+}
+const printPoint = function printPoint(x, y, r, color){
+    if (color){ color = "green"}
+    else {color = "red"}
+    ctx.beginPath();
+    ctx.arc(centerX + x / r * scale, centerY  - y / r * scale, 2, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.stroke();
 },
-
  resetXCheckboxes = function resetXCheckboxes() {
     const xCheckboxes = document.querySelectorAll('.x');
     xCheckboxes.forEach(checkbox => {
@@ -48,7 +118,7 @@ const canva = function canva(){
     });
 },
 
- state = {
+state = {
     siteX: 0,
     siteY: "0",
     siteR: 1.0,
@@ -114,59 +184,7 @@ document.getElementById("dataForm").addEventListener("submit", async (ev) => {
         if ((alternY) > goodYPlus || alternY < goodYMinus){
             throw new Error("bad y");
         }
-        const newRow = table.insertRow(CELL_INDEX.APPEND),
-        rowX = newRow.insertCell(CELL_INDEX.XIndex),
-        rowY = newRow.insertCell(CELL_INDEX.YIndex),
-        rowR = newRow.insertCell(CELL_INDEX.RIndex),
-        rowCurrentTime = newRow.insertCell(CELL_INDEX.CURRENT_TIME),
-        rowTime = newRow.insertCell(CELL_INDEX.TIME),
-        rowResult = newRow.insertCell(CELL_INDEX.RESULT),
-        results = { 
-            resX: state.siteX,
-            resY: state.siteY,
-            resR: state.siteR,
-            execTime: "",
-            currentTime: "",    
-            result: false,  
-        };
-        try{
-        const params = new URLSearchParams(state),
-         response = await fetch("/fcgi-bin/labwork1.jar", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: params.toString()
-        });
-        if (response.ok){
-            const result = await response.json();
-            results.currentTime = new Date(result.now).toLocaleString();
-            results.execTime = `${result.time} ns`
-            results.result = result.result.toString();
-        } else if (response.status === bedIndex) {
-            const result = await response.json();
-            results.currentTime = new Date(result.now).toLocaleString();
-            results.execTime = "N/A";
-            results.result = `error: ${result.reason}`;
-        } else {
-            results.currentTime = "N/A";
-            results.execTime = "N/A";
-            results.result = "error"
-        }
-        rowX.innerText = results.resX.toString();
-        rowY.innerText = results.resY.toString();
-        rowR.innerText = results.resR.toString();
-        rowCurrentTime.innerText = results.currentTime;
-        rowTime.innerText = results.execTime;
-        rowResult.innerText = results.result;
-        } catch{
-            rowX.innerText = results.resX.toString();
-            rowY.innerText = results.resY.toString();
-            rowR.innerText = results.resR.toString();
-            rowCurrentTime.innerText = "N/A";
-            rowTime.innerText = "N/A";
-            rowResult.innerText = "N/A";
-        }
+        checkPoint();
     } catch(ex){
         if (ex.message.includes("bad")){
             const time = 3000;
